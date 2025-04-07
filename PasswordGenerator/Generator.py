879b682f0ecf058d63f55a7f537d307aacbfe2f5
Sava Alexandru <savaalex1004@yahoo.com>
2025-04-07 12:07:32 +0300

    FirstTry

---
+++ PasswordGenerator/Generator.py
@@ -0,0 +1,103 @@
+
+import string
+import secrets
+import pickle
+
+def RandomGenerator (useDigits, useSymbol, length):
+    characters=string.ascii_letters
+    if(useDigits):
+        characters+=string.digits
+    elif(useSymbol):
+        characters+=string.punctuation
+    password=''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
+def Meniu():
+
+    print("\n..........................")
+    print("1.Cautare.")
+    print("2.Adaugare cont existent.")
+    print("3.Adauagre cont nou.")
+    print("4.Modificare.")
+    print("5.Stergere.")
+    print("6.Afisare totala")
+    print("7.Iesire.")
+    print("..........................")
+
+def AddNewAccount(storageAccounts):
+    id=input("Site/aplicatie:")
+    email=input("Username sau email:")
+    password=RandomGenerator(True, True,16)
+    storageAccounts[id]=[email,password]
+
+def AddExistingAccount(storageAccounts):
+    name=input("Site/aplicatie:")
+    email=input("Username sau email:")
+    password=input("Noua parola.")
+    storageAccounts[name]=[email,password]
+
+def SearchAccount(storageAccounts):
+    name=input("Ce cautati?")
+    if name in storageAccounts:
+        print(storageAccounts[name])
+    else:
+        print("Contul cautat nu este prezent.")
+
+def ChangeAccount(storageAccounts):
+    name=input("La ce cont faci modificari?")
+    if name not in storageAccounts:
+        print("Contul nu este prezent.")
+    else:
+        option=5
+        while(option!=4):
+            print("1.Username\n" \
+        "2.Password\n" \
+        "3.Contul intreg.\n" \
+        "4.Iesire.")
+            option=int(input("Ce doriti sa schimbati?"))
+            if option==1:
+                username=input("Username sau email:")
+                storageAccounts[name][0]=username
+            elif option ==2:
+                password=input("Noua parola.")
+                storageAccounts[name][1]=password
+            elif option==3:
+                username=input("Username sau email:")
+                password=input("Noua parola.")
+                storageAccounts[name]=[username,password]
+            else:
+                print("Exit.")
+                break
+def DeleteAccount(storageAccounts):
+    name=input("Ce cont stergi?")
+    if name in storageAccounts:
+        storageAccounts.pop(name)
+    else:
+        print("Contul cautat nu este prezent.")
+
+def ShowAll(storageAccounts):
+    for site, (email, password) in storageAccounts.items():
+        print(f"🔹 Site: {site} | 📧 Email: {email} | 🔑 Parolă: {password}")
+    
+def main():
+    storageAccounts={}
+    while(True):
+        Meniu()
+        option=int(input("Ce vrei sa faci?"))
+        if option==7:
+            print("Exit.")
+            break
+        elif option==1:
+            SearchAccount(storageAccounts)
+        elif option==2:
+            AddExistingAccount(storageAccounts)
+        elif option==3:
+            AddNewAccount(storageAccounts)
+        elif option==4:
+            ChangeAccount(storageAccounts)
+        elif option==5:
+            DeleteAccount(storageAccounts)
+        elif option==6:
+            print(storageAccounts)
+
+main()
